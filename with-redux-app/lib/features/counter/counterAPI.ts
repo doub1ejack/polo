39ad5fcb91029b805bf4aeb69a49af5c90f3741a
Micah Mutrux <micah.mutrux@gmail.com>
2024-03-17 12:07:25 -0400

    CONFIG - use env variables in api calls

--- with-redux-app/lib/features/counter/counterAPI.ts
+++ with-redux-app/lib/features/counter/counterAPI.ts
@@ -1,6 +1,8 @@
 // A mock function to mimic making an async request for data
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
+
 export const fetchCount = async (amount = 1) => {
-  const response = await fetch("http://localhost:3000/api/counter", {
+  const response = await fetch(`${apiDomain}/api/counter`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ amount }),
